af1fa666ff0b3d8abe08be2d2ebd48bf1964d91f
r.strzelczyk <robert.strzelczyk35a@gmail.com>
2019-11-25 11:46:41 +0100

    Add new section and modals

---
+++ js/script.js
@@ -0,0 +1,30 @@
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navHeaderId").style.paddingTop = ".5rem";
+        document.getElementById("navHeaderId").style.paddingBottom = ".5rem";
+        document.getElementById("logoId").style.fontSize = "1.5em";
+    } else {
+        document.getElementById("navHeaderId").style.paddingTop = "1.5rem";
+        document.getElementById("navHeaderId").style.paddingBottom = "1.5rem";
+        document.getElementById("logoId").style.fontSize = "1.75em";
+    }
+}
+
+var modals = document.getElementsByClassName('modal');
+var btns = document.getElementsByClassName("openModal");
+var spans = document.getElementsByClassName("close");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].onclick = function () {
+        modals[i].style.display = "block";
+    }
+}
+for (let i = 0; i < spans.length; i++) {
+    spans[i].onclick = function () {
+        modals[i].style.display = "none";
+    }
+}
