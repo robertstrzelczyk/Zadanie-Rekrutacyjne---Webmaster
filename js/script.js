789fa6134265e6b48b3503b99ea5232518b71db9
r.strzelczyk <robert.strzelczyk35a@gmail.com>
2019-11-27 23:02:33 +0100

    correct mobile responsive

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
 /*smaller nav section script*/
 function scrollFunction() {
     var wh = document.getElementById("navHeaderId");
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 || wh.style.width <= "765px" ) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navHeaderId").style.paddingTop = ".5rem";
         document.getElementById("navHeaderId").style.paddingBottom = ".5rem";
         document.getElementById("logoId").style.fontSize = "1.5em";
